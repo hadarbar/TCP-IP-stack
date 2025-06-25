9c416d4d3524909b746a58a911b05b480f1da201
Hadar Bar Aharoni <durdurbah@gmail.com>
2025-06-25 17:08:46 +0300

    add changes from redo

--- ethernet.py
+++ ethernet.py
@@ -1,24 +1,37 @@
-from typing import Union,Tuple
+from typing import Optional, Tuple
 from struct import unpack_from, calcsize
 
-BROADCAST = b"\xff\xff\xff\xff\xff\xff"
-PACKET_FORMAT_STRING = "6s6s2s"
-CRC_LEN = 4
+BROADCAST_MAC = b"\xff\xff\xff\xff\xff\xff"
+ETH_PACKET_FORMAT_STRING = "6s6s2s"
+ETHER_TYPES = {"ARP" : b"\x08\x06"}
+MAC_ADDR_LEN = 6
 
-def parse_eth_packet(raw_packet: bytes,
+def is_our_packet(raw_packet: bytes,
                  unicast_address: bytes,
-                 multicast_address: Union[bytes, None] = None)-> Union[Tuple[bytes, ...], None]:
+                 multicast_address: Optional[bytes] = None) -> bool:
     """
-    parses ethernet packets and returns the type and raw data of the dst mac corresponds to listening iface
-    :param raw_packet: raw packet bytes
-    :param unicast_address: the ifaces unicast address
-    :param multicast_address: the ifaces multicast address
-    :return: tuple with dst mac, src mac, type and packet data, if dst mac is ifaces, else return none
+    check if packet dst mac matches our mac address
     """
-    headers_size = calcsize(PACKET_FORMAT_STRING)
-    dst_mac, src_mac, ether_type = unpack_from(PACKET_FORMAT_STRING, raw_packet)
-    valid_addresses = [BROADCAST, unicast_address, multicast_address]
-    if dst_mac in valid_addresses:
-        return dst_mac, src_mac, ether_type, raw_packet[headers_size:-CRC_LEN]
+    our_addresses = [BROADCAST_MAC, unicast_address, multicast_address]
+    if raw_packet and raw_packet[:MAC_ADDR_LEN] in our_addresses:
+        return True
+    else:
+        return False
 
-    return None
+def parse_eth_frame(raw_packet: bytes)-> Tuple[bytes, ...]:
+    """
+    parses ethernet packets and returns the type and raw data
+    :param raw_packet: raw packet bytes
+    :return: tuple
+    """
+    headers_size = calcsize(ETH_PACKET_FORMAT_STRING)
+    dst_mac, src_mac, ether_type = unpack_from(ETH_PACKET_FORMAT_STRING, raw_packet)
+    return dst_mac, src_mac, ether_type, raw_packet[headers_size:]
+
+def make_eth_packet(dst_mac: bytes, src_mac: bytes, ether_type: bytes, data: bytes) -> bytes:
+    """
+    build ether packet and return bytes of full packet
+    """
+
+    packet = dst_mac + src_mac + ether_type + data
+    return packet
