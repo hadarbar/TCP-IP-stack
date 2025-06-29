b269b3824880a6c8cbf1dd5a10669a3a346e71bf
Hadar Bar Aharoni <durdurbah@gmail.com>
2025-06-29 19:16:14 +0300

    add changes to main

--- main.py
+++ main.py
@@ -3,17 +3,25 @@ from scapy.all import conf, get_if_hwaddr
 import arp
 import ethernet
 
+def print_arps(sock: conf.L2socket, my_mac: str):
+    """
+    loop that listens on socket and prints the arp packets
+    """
+    while True:
+        _, frame, _ = sock.recv_raw()
+        if not ethernet.is_our_packet(frame, bytes.fromhex(my_mac.replace(":", ""))):
+            continue
+        dst_mac, src_mac, ether_type, data = ethernet.parse_eth_packet(frame)
+        if ether_type == arp.ARP_ETHER_TYPE:
+            arp.print_arp(data)
+
 
 def main():
     iface = "Intel(R) Wi-Fi 6 AX201 160MHz"
     sock = conf.L2socket(iface=iface, promisc=True)
     my_mac = get_if_hwaddr(iface)
-    while True:
-        recv = sock.recv_raw()
-        if ethernet.is_our_packet(recv[1], bytes.fromhex(my_mac.replace(":", ""))):
-            dst_mac, src_mac, ether_type, data = ethernet.parse_eth_packet(recv[1])
-            if ether_type == arp.ARP_ETHER_TYPE:
-                arp.print_arp(data)
+    arp.send_arp_request(sock, "10.0.0.138", "10.0.0.9", my_mac)
+    print_arps(sock, my_mac)
 
 
 if __name__ == "__main__":
